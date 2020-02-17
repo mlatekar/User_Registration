06d7b8c6a17e59df3407bf89ea823954a7e76b70
mangesh <mlatekar@gmail.com>
2020-02-17 18:27:27 +0530

    ADDED:Welcome to User Registration

---
+++ src/main/java/com/Registration/RegisterValidation.java
@@ -0,0 +1,7 @@
+package com.Registration;
+
+public class RegisterValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration");
+    }
+}
---
+++ src/test/java/com/Registration/UserRegistrationTest.java
@@ -0,0 +1,7 @@
+package com.Registration;
+
+import org.junit.Test;
+
+public class UserRegistrationTest {
+
+}
