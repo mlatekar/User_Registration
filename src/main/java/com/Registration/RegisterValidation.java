1544b9cceb955ca80e76685c33e4e74962ecd1fd
mangesh <mlatekar@gmail.com>
2020-02-18 11:22:44 +0530

    ADDED:Valid first name

--- src/main/java/com/Registration/RegisterValidation.java
+++ src/main/java/com/Registration/RegisterValidation.java
@@ -1,7 +1,13 @@
 package com.Registration;
 
+import java.util.regex.Pattern;
+
 public class RegisterValidation {
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration");
     }
+    private static final String firstName="^[A-Z]{1}[a-zA-Z]{3,}$";
+
+    public static boolean validFirstName(String fName) { return Pattern.matches(firstName,fName); }
 }
+
--- src/test/java/com/Registration/UserRegistrationTest.java
+++ src/test/java/com/Registration/UserRegistrationTest.java
@@ -1,7 +1,18 @@
 package com.Registration;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean valid=RegisterValidation.validFirstName("Mangesh");
+        Assert.assertEquals(true,valid);
+    }
+    @Test
+    public void givenFirstName_WhenUnProper_ShouldReturnFalse() {
+        boolean valid=RegisterValidation.validFirstName("ma");
+        Assert.assertEquals(false,valid);
+    }
 
 }
